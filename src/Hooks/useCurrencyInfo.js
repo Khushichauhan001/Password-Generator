1c72291060298b1471a77d2b36531ccedb732171
Khushichauhan01 <khushichauhan2506@gmail.com>
2025-02-27 10:56:34 +0530

    first commit

--- src/Hooks/useCurrencyInfo.js
+++ src/Hooks/useCurrencyInfo.js
@@ -2,7 +2,7 @@ import { useEffect , useState } from "react";
 
 
 function useCurrencyInfo(currency){
-    const [data , setData] = useState({}) // useState k andr hum ek empty object dal dete h ki agr fetch call ni aarhi h toh koi error na aaye 
+    const [data , setData] = useState(null) // useState k andr hum ek empty object dal dete h ki agr fetch call ni aarhi h toh koi error na aaye 
 
     useEffect(() => {
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
